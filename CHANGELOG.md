8326d8851d44ac134df278773634367e10c0bac6
Nhan Cao <nhancv92@gmail.com>
2020-09-19 10:47:41 +0700

    tag: 1.0.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## [1.0.1] - September 19, 2020.
+
+* Update comments.
+
 ## [1.0.0] - September 19, 2020.
 
 * Initial release.
--- lib/nft_local_storage.dart
+++ lib/nft_local_storage.dart
@@ -2,12 +2,16 @@ library nft_local_storage;
 
 import 'package:shared_preferences/shared_preferences.dart';
 
+/// This class for local storage using SharedPreferences
 class LocalStorage {
+  // Create new Future ref to SharedPreferences instance
   final Future<SharedPreferences> sharedPrefs = SharedPreferences.getInstance();
 
   /// Save data with primary data types: String, List<String>, double, int, bool
   Future<bool> saveData<T>(String key, T value) async {
+    // Get SharedPreferences ref
     final SharedPreferences prefs = await sharedPrefs;
+    // With primary data type, save to local
     if (T is String) {
       return prefs.setString(key, value as String);
     } else if (T is bool) {
@@ -19,12 +23,15 @@ class LocalStorage {
     } else if (T is List<String>) {
       return prefs.setStringList(key, value as List<String>);
     }
+    // Default return false
     return false;
   }
 
   // Get data by key
   Future<T> getData<T>(String key) async {
+    // Get SharedPreferences ref
     final SharedPreferences prefs = await sharedPrefs;
+    // Get data by key with cast to output data type
     return prefs.get(key) as T;
   }
 }
